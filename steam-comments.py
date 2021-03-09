8c152c1c467eabd42fe9d0bb1fdb64807b02c52e
elsorino <elso@pm.me>
2021-03-09 00:29:01 -0500

    Changed to use arguments and file outputs

--- steam-comments.py
+++ steam-comments.py
@@ -1,23 +1,35 @@
+#!/usr/bin/env python3
 import requests
-from bs4 import BeautifulSoup
 import re
+import argparse
+from bs4 import BeautifulSoup
 
-url = input("Please paste comment url, example: https://steamcommunity.com/id/sargentyen/allcomments\n")
-allcomments = "allcomments"
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--profile", required=True, help = "Comment url of steam profile to use")
+parser.add_argument("-n", "--numbers", required=True, help = "Number of comment pages to use")
+parser.add_argument("-o", "--output", required=True, help = "File to direct output")
+args = parser.parse_args()
+url = args.profile
+
 if "allcomments" in url:
     pass
 else:
     print("\nInvalid url entered, exiting")
     quit()
 
-pagenumbers = int(input("How many pages of comments would you like to download?\n"))
+url +="?ctp=" #Needed for page numbers
 
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find('div', class_='maincontent') #Where all comments are located
-steam_elems = results.find_all('div', class_='commentthread_comments') #Finds all comments
+pagenumbers = int(args.numbers)
+currentnumber = 1 #Start with first page
 
-for all in steam_elems:
-    test = re.sub(r'([\r\n\t])+', r'\n', all.text) #Removes a bunch of blank lines
-    print(test)
+while currentnumber <= pagenumbers:
+    currentpage = url + str(currentnumber)
+    page = requests.get(currentpage)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find('div', class_='maincontent') #Where all comments are located
+    steam_elems = results.find_all('div', class_='commentthread_comments') #Finds all comments
 
+    for all in steam_elems:
+        test = re.sub(r'([\r\n\t])+', r'\n', all.text) #Removes a bunch of blank lines
+        print(test, file=open(args.output,"a+"))
+    currentnumber += 1
