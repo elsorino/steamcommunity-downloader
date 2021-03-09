e4ae050f96429ed3cd06931f29924b648bb21776
elsorino <elso@pm.me>
2021-03-09 18:28:16 -0500

    Added support for steam groups

--- README.md
+++ README.md
@@ -11,12 +11,20 @@ Script I made while learning Python
 
 ### Usage
 
+<<<<<<< HEAD
 `./steam-comments.py -p https://steamcommunity.com/id/ChetFaliszek/allcomments -n 2 -o example.txt`
+=======
+`./steam-comments.py -p https://steamcommunity.com/id/ChetFaliszek/ -p 2 -o example.txt`
+>>>>>>> 512c8bf (Added support for steam groups)
 
 Profile must be public(for now)
 
 TODO:
 
 * Support private profiles
+<<<<<<< HEAD
 * Support steam group comments
 * Support steam emotes. Currently emotes are blanked out
+=======
+* Support steam emotes. Currently emotes are blanked out
+>>>>>>> 512c8bf (Added support for steam groups)
--- steam-comments.py
+++ steam-comments.py
@@ -5,19 +5,17 @@ import argparse
 from bs4 import BeautifulSoup
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--profile", required=True, help = "Comment url of steam profile to use")
+parser.add_argument("-p", "--profile", required=True, help = "URL of steam profile or group to use")
 parser.add_argument("-n", "--numbers", required=True, help = "Number of comment pages to use")
 parser.add_argument("-o", "--output", required=True, help = "File to direct output")
 args = parser.parse_args()
 url = args.profile
 
-if "allcomments" in url:
-    pass
+if "steamcommunity" in url:
+    url +="allcomments?ctp="
 else:
     print("\nInvalid url entered, exiting")
     quit()
-
-url +="?ctp=" #Needed for page numbers
 
 pagenumbers = int(args.numbers)
 currentnumber = 1 #Start with first page
@@ -26,7 +24,7 @@ while currentnumber <= pagenumbers:
     currentpage = url + str(currentnumber)
     page = requests.get(currentpage)
     soup = BeautifulSoup(page.content, 'html.parser')
-    results = soup.find('div', class_='maincontent') #Where all comments are located
+    results = soup.find('div', class_='commentthread_comment_container') #Where all comments are located
     steam_elems = results.find_all('div', class_='commentthread_comments') #Finds all comments
 
     for all in steam_elems:
